7fcc860155a8ca04de67f490a4a014992ec6a607
Amr Gharseldin <gharseldin@aucegypt.edu>
2018-05-08 14:19:44 -0500

    Initial Commit

---
+++ Chapter1QuestionAnswers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chapter1QuestionAnswers
+//
+//  Created by Amr Gharseldin on 5/8/18.
+//  Copyright © 2018 Amr Gharseldin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
